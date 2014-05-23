c1dd9d229019d97cebe736655a13a363fcf085a7
Ryan Ly <windask@hotmail.com>
2014-05-22 21:36:05 -0400

    experiments with templating

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ var express = require('express');
 var app  = express();
 var server = require('http').createServer(app);
 var path = require('path');
+var fs = require('fs');
 
-app.use(express.static(path.join(__dirname, '')));
+eval(fs.readFileSync('./js/testData.ejs')+'');
+
+app.use(express.static(path.join(__dirname, '/')));
 app.use(express.static('./css'));
 app.use(express.static('./js'));
 
+app.set('view engine', 'html');
+
+app.engine('.html', require('ejs').__express);
 
 app.get('/', function(req, res) {
-    res.sendfile('./index.htm');
+      res.render('index', {
+    title: title,});
  });
+app.get('/test', function(req, res) {
+      res.render('users', {
+    users: users,});
+ });
+
 
 app.listen(process.env.PORT || 7000)
 
