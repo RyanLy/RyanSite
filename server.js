75ae5014b878829d9d480b0629c494ac0362e62f
Ryan Ly <windask@hotmail.com>
2014-09-17 16:37:53 -0400

    UI improvements

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 require('newrelic');
 var express = require('express');
 var app  = express();
-var server = require('http').createServer(app);
 var path = require('path');
-var fs = require('fs');
-
-eval(fs.readFileSync('./js/testData.ejs')+'');
 
 app.use(express.static(path.join(__dirname, '/')));
 app.use(express.static('./css'));
@@ -16,12 +12,7 @@ app.set('view engine', 'html');
 app.engine('.html', require('ejs').__express);
 
 app.get('/', function(req, res) {
-      res.render('index', {
-    title: title,});
- });
-app.get('/test', function(req, res) {
-      res.render('users', {
-    users: users,});
+      res.render('index');
  });
 
 app.listen(process.env.PORT || 7000)
