4c4ea35e1c6e71c16e716967e7ef52493a3c92a0
Ryan Ly <windask@hotmail.com>
2015-05-14 18:55:16 -0400

    Updated site temporarily

--- server.js
+++ server.js
@@ -26,30 +26,6 @@ app.post('/message',
 			res.send(405,err);
 		}
 		else{
-			if (email != ""){
-				if (email.indexOf("@") > -1 ){
-					res.send("Thank you " + name + " for your message!\nA verification email will be sent to " + email);
-					var mailOptions = {
-					    from: "Ryan Ly<ryan@ryanly.ca>", // sender address
-					    to: email, // list of receivers
-					    subject: "Verification Email", // Subject line
-					    text: "Thank you for your message. This is an email to verify your message to Ryan.\nMessage: \"" + message + "\"", // plaintext body
-					}
-					smtpTransport.sendMail(mailOptions, function(error, response){
-					    if(error){
-					        console.log(error);
-					    }else{
-					        console.log("Message sent: " + response.message);
-					    }
-					});
-				}
-				else{
-					res.send("Please use a valid email!");
-				}
-			}
-			else{
-    			res.send("Thank you " + name + " for your message!");
-			}
 			var mailOptions = {
 			    from: "Ryan Ly<ryan@ryanly.ca>", // sender address
 			    to: "ryan@ryanly.ca", // list of receivers
@@ -59,8 +35,10 @@ app.post('/message',
 			smtpTransport.sendMail(mailOptions, function(error, response){
 			    if(error){
 			        console.log(error);
+			        res.send("There was an error with sending your message!");
 			    }else{
 			        console.log("Message sent: " + response.message);
+			        res.send("Thank you " + name + " for your message!");
 			    }
 			});
     	}
