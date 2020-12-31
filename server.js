d980c0b9692672bb90bb7750fbd4da91ec60fda1
Ryan Ly <windask@hotmail.com>
2020-12-31 13:15:39 +0000

    MC Endpoints

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var path = require('path');
 var app  = express();
 var smtpTransport = require("./js/mailer");
 var favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const https = require('https');
+const request = require('request')
+var proxy = require('express-http-proxy');
 
+app.use(bodyParser.json());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,7 +22,7 @@ app.get('/', function(req, res) {
       res.render('index');
  });
 
-app.post('/message', 
+app.post('/message',
 	function(req, res) {
 		name = req.param('name');
 		message = req.param('message');
@@ -43,6 +49,105 @@ app.post('/message',
 			});
     	}
 });
+
+app.post('/start-minecraft', (req, res) => {
+	const key = req.body.key;
+
+	if (key === process.env.MINECRAFT_KEY) {
+		exec('(cd ~/Aws-MC-Scripts && ./startMC.sh)', (err, stdout, stderr) => {
+			console.log(err);
+			console.log(stderr);
+			console.log(stdout);
+
+			res.send({
+				success: true
+			})
+		});
+	} else {
+		res.send({
+			success: false
+		})
+	}
+});
+
+app.post('/stop-minecraft', (req, res) => {
+	const key = req.body.key;
+
+	if (key === process.env.MINECRAFT_KEY) {
+		exec('(cd ~/Aws-MC-Scripts && ./stopMC.sh)', (err, stdout, stderr) => {
+			console.log(err);
+			console.log(stderr);
+			console.log(stdout);
+
+			res.send({
+				success: true
+			})
+		});
+	} else {
+		res.send({
+			success: false
+		})
+	}
+});
+
+app.post('/new-minecraft', (req, res) => {
+	const key = req.body.key;
+
+	if (key === process.env.MINECRAFT_KEY) {
+		exec('(cd ~/Aws-MC-Scripts && ./newWorld.sh)', (err, stdout, stderr) => {
+			console.log(err);
+			console.log(stderr);
+			console.log(stdout);
+
+			res.send({
+				success: true
+			})
+		});
+	} else {
+		res.send({
+			success: false
+		})
+	}
+});
+
+app.post('/minecraft-status', (req, res) => {
+	const key = req.body.key;
+
+	if (key === process.env.MINECRAFT_KEY) {
+		exec(`ssh -t ubuntu@minecraft.ryanly.ca -o ConnectTimeout=3 'tmux send-keys -t Minecraft.0 "list" ENTER; sleep 0.1; tmux capture-pane -t Minecraft.0 -p | tail -2 | head -1 | pcregrep -o1 "There are (.*) of a max"'`, (err, stdout, stderr) => {
+			console.log(stderr);
+
+			console.log(`Online: ${stdout}`);
+
+			res.send({
+				success: true,
+				online: stdout.trim()
+			})
+		});
+	} else {
+		res.send({
+			success: false
+		})
+	}
+});
+
+// app.post('/toggle-lights', (req, res) => {
+// 	const key = req.body.key;
+
+// 	if (key === 'raspberry') {
+// 		request.post('http://home.ryanly.ca:3000/toggle-lights', {}, () => {
+// 			res.send({
+// 				success: false
+// 			});
+// 		});
+// 	}
+// })
+app.use('/proxy', proxy('home.ryanly.ca:3000', {
+  filter: function(req) {
+    return req.body.key == 'raspberry';
+ 	}
+}));
+
 
 app.listen(process.env.PORT || 7000)
 
